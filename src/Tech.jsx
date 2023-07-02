c137960bb9012b33b510eabf4f89cac8775f438e
Francisco Ferreira <franciscoferrefc@gmail.com>
2023-07-03 01:53:20 +0200

    Href changes

--- src/Tech.jsx
+++ src/Tech.jsx
@@ -7,27 +7,27 @@ export default function Tech() {
         <div className="flex justify-center">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="flex justify-center items-center gap-x-8 gap-y-10">
-                    <a href="https://www.lifullconnect.com/">
+                    <a href="#">
                         <img
-                            className="max-h-24 w-full object-contain"
+                            className="max-h-28 w-full object-contain grayscale hover:grayscale-0"
                             src={golang}
                             alt="Transistor"
                             width={158}
                             height={60}
-                        />              
+                        />        
                     </a>
-                    <a href="https://www.lifullconnect.com/">
+                    <a href="#">
                         <img
-                            className="max-h-16 w-full object-contain"
+                            className="max-h-16 w-full object-contain grayscale hover:grayscale-0"
                             src={php}
                             alt="Reform"
                             width={158}
                             height={60}
                         />
                     </a>
-                    <a href="https://www.lifullconnect.com/">
+                    <a href="#">
                         <img
-                            className="max-h-16 w-full object-contain"
+                            className="max-h-16 w-full object-contain grayscale hover:grayscale-0"
                             src={kotlin}
                             alt="Tuple"
                             width={158}
